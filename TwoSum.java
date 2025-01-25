fea3b6139aa562d4d19f8a087c274d2a6ffc4f60
kasrazunnaiyyer <kasrazunnaiyyer@outlook.com>
2025-01-25 18:02:36 +0500

    two Sum

---
+++ TwoSum.java
@@ -0,0 +1,24 @@
+import java.util.HashMap;
+
+public class TwoSum {
+    public int[] twoSum(int[] nums, int target){
+
+        // Creating hashmap to store all the values and there indexes as Key and Value
+        HashMap<Integer,Integer> map = new HashMap<>();
+
+        // iterating through the list and storing it in the Hashmap ap comparing it along the way
+        for(int i = 0; i< nums.length; i++){
+
+            // calculating the complement value
+            int complement  = target - nums[i];
+
+            //if the value dose exists then it will return the new int array
+            if(map.containsKey(complement)){
+                return new int[] {map.get(complement),i};
+            }
+            map.put(nums[i],i);
+        }
+
+        return new int[] {};
+    }
+}
