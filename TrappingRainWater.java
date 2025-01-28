f39a4f34ce06f816539eda2c582e1d601281d204
kasrazunnaiyyer <kasrazunnaiyyer@outlook.com>
2025-01-28 10:32:36 +0500

    Trapping Water

--- TrappingRainWater.java
+++ TrappingRainWater.java
@@ -1,14 +1,21 @@
 public class TrappingRainWater {
 
     public static int trappingWater(int[] numbs){
+
+        //initializing left pointer and right pointer
         int left = 0;
         int right = numbs.length -1;
 
+        // declaring the values for total left max and right max
         int total = 0;
         int leftmax= numbs[left];
         int rightmax = numbs[right];
 
+
+        // checking if the left pointer is less the right pointer
         while(left<right){
+
+            // if the value of left pointer is less than right pointer we'll apply the formula l[max] - current height
             if(numbs[left]< numbs[right]){
                 leftmax = Math.max(leftmax, numbs[left]);
                 if(leftmax - numbs[left] > 0){
@@ -16,6 +23,7 @@ public class TrappingRainWater {
                 }
                 left++;
             }
+            // else doing the same thing for right
             else{
                 rightmax = Math.max(rightmax, numbs[right]);
                 if(rightmax - numbs[right] > 0 ){
@@ -25,6 +33,8 @@ public class TrappingRainWater {
             }
 
         }
+
+        // returning total value
         return total;
     }
 
