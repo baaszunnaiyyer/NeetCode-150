c61e5646cf7f7273e403ef9513fc358140851ae0
Baas Zunnaiyyer <85397563+baaszunnaiyyer@users.noreply.github.com>
2025-04-06 11:22:13 +0500

    Reorder Linked list

--- ReorderLinkedList.java
+++ ReorderLinkedList.java
@@ -1,2 +1,47 @@
-package PACKAGE_NAME;public class ReorderLinkedList {
+public class ReorderLinkedList {
+
+    class ListNode{
+        int val;
+        MergeTowList.ListNode next;
+        ListNode(){}
+        ListNode(int val) {this.val =val;}
+        ListNode(int val, MergeTowList.ListNode next) {this.val = val; this.next = next;}
+    }
+
+    public void reorderList(ListNode head){
+        if(head == null){
+            return;
+        }
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        ListNode prev = null,  curr = slow, temp;
+
+        while( curr != null){
+            temp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = temp;
+        }
+
+        ListNode first = head, second = prev;
+
+        while(second.next != null){
+            temp = first.next;
+            first.next = second;
+            first = temp;
+
+            temp = second.next;
+            second.next = first;
+            second = temp;
+        }
+    }
+
+
 }
